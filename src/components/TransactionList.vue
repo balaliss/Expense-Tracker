3f01be176501f69763a768a22c50d14fea6dda8f
balaliss <balalis43@gmail.com>
2023-11-13 11:16:21 -0500

    Completed Project.
    Now you can store your transactions.

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,22 +1,32 @@
 <template>
-<h3>History</h3>
-      <ul id="list" class="list">
-        <li v-for="transactions in transactions"></li> 
-          Cash <span>-$400</span><button class="delete-btn">x</button>
-      </ul>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button class="delete-btn" @click="deleteTransaction(transaction.id)">
+        x
+      </button>
+    </li>
+  </ul>
 </template>
 
-<script>
-  export default {
-    data(){
-      return {
-        transactions: [
-          { id: 1, text: 'Flower', amount: -19.99},
-          { id: 2, text: 'Salary', amount: 299.97},
-          { id: 3, text: 'Book', amount: -19.99},
-          { id: 4, text: 'Camera', amount: 299.97},
-        ]
-      }
-    }
-  }
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+
+const emit = defineEmits(['transactionDeleted']);
+
+const deleteTransaction = (id) => {
+  emit('transactionDeleted', id);
+};
 </script>
